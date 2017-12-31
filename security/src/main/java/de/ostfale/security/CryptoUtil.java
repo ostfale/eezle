df1cb69ed60fbc9a5276aa89a2cfae0d94048b0f
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2017-12-31 23:20:52 +0100

    XML Digital Signature
    + add crypto util functions
    + add digital signature functions for XML

---
+++ security/src/main/java/de/ostfale/security/CryptoUtil.java
@@ -0,0 +1,102 @@
+package de.ostfale.security;
+
+import java.io.*;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
+/**
+ * some cryptography utility functions
+ * Created by Uwe Sauerbrei on 31.12.2017
+ */
+public class CryptoUtil {
+
+    public static final String ALGORITHM = "RSA";
+    public static final Integer KEY_LENGTH = 1024;
+
+    public PrivateKey getStoredPrivateKey(String filePath) {
+        byte[] keydata = getKeyData(filePath);
+        PKCS8EncodedKeySpec encodedPrivateKey = new PKCS8EncodedKeySpec(keydata);
+        try {
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            return keyFactory.generatePrivate(encodedPrivateKey);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public PublicKey getStoredPublicKey(String filePath) {
+        byte[] keydata = getKeyData(filePath);
+        try {
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            X509EncodedKeySpec encodedPublicKey = new X509EncodedKeySpec(keydata);
+            return keyFactory.generatePublic(encodedPublicKey);
+        } catch (NoSuchAlgorithmException | NullPointerException | InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void storeKeyPairs(String dirPath) {
+        KeyPair keyPair = generateKeyPairs();
+        PrivateKey privateKey = keyPair.getPrivate();
+        PublicKey publicKey = keyPair.getPublic();
+        storeKeys(dirPath + File.separator + "publickey.key", publicKey);
+        storeKeys(dirPath + File.separator + "privatekey.key", privateKey);
+    }
+
+    private KeyPair generateKeyPairs() {
+        try {
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance(ALGORITHM);
+            keyGen.initialize(KEY_LENGTH);
+            return keyGen.generateKeyPair();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private void storeKeys(String filePath, Key key) {
+        byte[] keyBytes = key.getEncoded();
+        OutputStream outStream = null;
+        try {
+            outStream = new FileOutputStream(filePath);
+            outStream.write(keyBytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (outStream != null) {
+                try {
+                    outStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private byte[] getKeyData(String filePath) {
+        File file = new File(filePath);
+        byte[] buffer = new byte[(int) file.length()];
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(file);
+            fis.read(buffer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return buffer;
+    }
+
+}
---
+++ security/src/main/java/de/ostfale/security/DigitalSignatureGenerator.java
@@ -0,0 +1,96 @@
+package de.ostfale.security;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.crypto.MarshalException;
+import javax.xml.crypto.dsig.*;
+import javax.xml.crypto.dsig.dom.DOMSignContext;
+import javax.xml.crypto.dsig.keyinfo.KeyInfo;
+import javax.xml.crypto.dsig.keyinfo.KeyInfoFactory;
+import javax.xml.crypto.dsig.keyinfo.KeyValue;
+import javax.xml.crypto.dsig.spec.C14NMethodParameterSpec;
+import javax.xml.crypto.dsig.spec.TransformParameterSpec;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.*;
+import java.util.Collections;
+
+/**
+ * This class is used to provide convenient methods to digitally sign an XML  document.
+ * Created by Uwe Sauerbrei on 31.12.2017
+ */
+public class DigitalSignatureGenerator {
+
+    public void generateXMLDigitalSignature(String originalXmlFilePath, String destnSignedXmlFilePath, String privateKeyFilePath, String publicKeyFilePath) {
+        // get XML original document
+        Document document = getXmlDocument(originalXmlFilePath);
+        // create XML signature factory
+        XMLSignatureFactory xmlSignatureFactory = XMLSignatureFactory.getInstance("DOM");
+        PrivateKey privateKey = new CryptoUtil().getStoredPrivateKey(privateKeyFilePath);
+        DOMSignContext domSignContext = new DOMSignContext(privateKey, document.getDocumentElement());
+        try {
+            Reference reference = xmlSignatureFactory.newReference("", xmlSignatureFactory.newDigestMethod(DigestMethod.SHA1, null),
+                    Collections.singletonList(xmlSignatureFactory.newTransform(Transform.ENVELOPED,
+                            (TransformParameterSpec) null)), null, null);
+            SignedInfo signedInfo = xmlSignatureFactory.newSignedInfo(
+                    xmlSignatureFactory.newCanonicalizationMethod(CanonicalizationMethod.INCLUSIVE,
+                            (C14NMethodParameterSpec) null),
+                    xmlSignatureFactory.newSignatureMethod(SignatureMethod.RSA_SHA1, null),
+                    Collections.singletonList(reference));
+            // pass the public key file path
+            KeyInfo keyInfo = getKeyInfo(xmlSignatureFactory, publicKeyFilePath);
+            // create new xml signature
+            XMLSignature xmlSignature = xmlSignatureFactory.newXMLSignature(signedInfo, keyInfo);
+            // sign the document
+            xmlSignature.sign(domSignContext);
+            storeSignedDocument(document, destnSignedXmlFilePath);
+
+        } catch (NoSuchAlgorithmException | InvalidAlgorithmParameterException | XMLSignatureException | MarshalException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private Document getXmlDocument(String xmlFilePath) {
+        DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+        builderFactory.setNamespaceAware(true);
+        try {
+            return builderFactory.newDocumentBuilder().parse(new FileInputStream(xmlFilePath));
+        } catch (SAXException | IOException | ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private KeyInfo getKeyInfo(XMLSignatureFactory xmlSignatureFactory, String publicKeyPath) {
+        PublicKey publicKey = new CryptoUtil().getStoredPublicKey(publicKeyPath);
+        KeyInfoFactory keyInfoFactory = xmlSignatureFactory.getKeyInfoFactory();
+        try {
+            KeyValue keyValue = keyInfoFactory.newKeyValue(publicKey);
+            return keyInfoFactory.newKeyInfo(Collections.singletonList(keyValue));
+        } catch (KeyException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private void storeSignedDocument(Document document, String destnSignedXmlFilePath) {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        try {
+            Transformer transformer = transformerFactory.newTransformer();
+            StreamResult streamResult = new StreamResult(new File(destnSignedXmlFilePath));
+            transformer.transform(new DOMSource(document), streamResult);
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        }
+    }
+}
