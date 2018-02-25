c9f6ce8719509ce36ed363c0d65728926e9ce964
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2018-02-25 19:16:34 +0100

    Design with Lambdas
    + use predicate for a filter
    + strategy pattern

---
+++ lambdastream/src/designWithLambda/Asset.java
@@ -0,0 +1,28 @@
+package de.ostfale.fp.designWithLambda;
+
+/**
+ * Created by Uwe Sauerbrei on 25.02.2018
+ */
+public class Asset {
+
+    public enum AssetType {BOND, STOCK}
+
+    private final AssetType type;
+    private final int value;
+
+    public Asset(AssetType type, int value) {
+        this.type = type;
+        this.value = value;
+    }
+
+    public AssetType getType() {
+        return type;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    private void doIt() {
+    }
+}
