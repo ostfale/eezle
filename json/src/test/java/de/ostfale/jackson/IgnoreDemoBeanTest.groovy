0a2134fd68605e7d67c7e470a09b23c526dea9d5
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2018-04-22 19:24:55 +0200

    Created Spock tests
    + fixed dependencies
    + created spock tests from JUnit tests

--- json/src/test/java/de/ostfale/jackson/IgnoreDemoBeanTest.groovy
+++ json/src/test/java/de/ostfale/jackson/IgnoreDemoBeanTest.groovy
@@ -1,5 +1,6 @@
 package de.ostfale.jackson
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import spock.lang.Specification
 import spock.lang.Subject
 import spock.lang.Title
@@ -10,4 +11,32 @@ import spock.lang.Title
 @Title('Unit test for JsonIgnore annotation')
 @Subject(IgnoreDemoBean)
 class IgnoreDemoBeanTest extends Specification {
+
+    private ObjectMapper objectMapper
+
+    void setup() {
+        objectMapper = new ObjectMapper()
+    }
+
+    void cleanup() {
+        objectMapper = null
+    }
+
+    def "testSerializingWithJsonIgnore"() {
+        when: "Class IgnoreDemoBean is serialized"
+        String jsonString = objectMapper.writeValueAsString(new IgnoreDemoBean())
+        then: "Attribut personId is not serialized"
+        jsonString.contains("James Clark")
+        !jsonString.contains("personId")
+    }
+
+    def "testDeserializingWithJsonIgnore"() {
+        given: "A json input string"
+        String jsonString = '{"personId": 231, "name": "Mary Parker"}'
+        when: "Json is deserialized to class"
+        IgnoreDemoBean bean = objectMapper.readValue(jsonString, IgnoreDemoBean.class)
+        then: "Ignored attriutes have not been deserialized"
+        bean.getName() == "Mary Parker"
+        bean.getPersonId() != 231L
+    }
 }
