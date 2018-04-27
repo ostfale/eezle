e119fcde097595762e22ddc46ac59bffe5716807
ostfale <info@jug-hh.de>
2018-04-27 08:18:02 +0200

    TestNG - Run tests in parallel
    + added test and configuration xml file

---
+++ testfw/src/test/java/de/ostfale/testng/TestNG_TestParallel1.java
@@ -0,0 +1,26 @@
+package de.ostfale.testng;
+
+import org.testng.annotations.Test;
+
+/**
+ * First test class for parallel execution
+ * Created : 27.04.2018
+ *
+ * @author : usauerbrei
+ */
+public class TestNG_TestParallel1 {
+
+	@Test
+	public void testMethod1() throws InterruptedException {
+		System.out.println("TestNG_Parallel1 -> testMethod1");
+		Thread.sleep(6000);
+		System.out.println("TestNG_Parallel1 -> testMethod1 -> More Steps");
+	}
+
+	@Test
+	public void testMethod2() throws InterruptedException {
+		System.out.println("TestNG_Parallel1 -> testMethod2");
+		Thread.sleep(6000);
+		System.out.println("TestNG_Parallel1 -> testMethod2 -> More Steps");
+	}
+}
---
+++ testfw/src/test/java/de/ostfale/testng/TestNG_TestParallel2.java
@@ -0,0 +1,26 @@
+package de.ostfale.testng;
+
+import org.testng.annotations.Test;
+
+/**
+ * Second test class for parallel execution
+ * Created : 27.04.2018
+ *
+ * @author : usauerbrei
+ */
+public class TestNG_TestParallel2 {
+
+	@Test
+	public void testMethod1() throws InterruptedException {
+		System.out.println("TestNG_Parallel2 -> testMethod1");
+		Thread.sleep(6000);
+		System.out.println("TestNG_Parallel2 -> testMethod1 -> More Steps");
+	}
+
+	@Test
+	public void testMethod2() throws InterruptedException {
+		System.out.println("TestNG_Parallel2 -> testMethod2");
+		Thread.sleep(6000);
+		System.out.println("TestNG_Parallel2 -> testMethod2 -> More Steps");
+	}
+}
