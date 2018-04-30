99da9c70abd632f9cd90e61c90659dea190b028d
ostfale <info@jug-hh.de>
2018-04-30 08:35:33 +0200

    TestNG - ITestResult
    + use above interface to get listend to the result of each tests and do some appropriate action

---
+++ testfw/src/test/java/de/ostfale/testng/listener/TestNG_ITestResultDemo.java
@@ -0,0 +1,37 @@
+package de.ostfale.testng.listener;
+
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+/**
+ * Figure out the status ot the last run test
+ * Created : 30.04.2018
+ *
+ * @author : usauerbrei
+ */
+public class TestNG_ITestResultDemo {
+
+	@Test
+	public void testMethod1() {
+		System.out.println("Running -> testMethod1");
+		Assert.assertTrue(false);
+	}
+
+	@Test
+	public void testMethod2() {
+		System.out.println("Running -> testMethod2");
+		Assert.assertTrue(true);
+	}
+
+	@AfterMethod
+	public void afterMethod(ITestResult testResult) {
+		if (testResult.getStatus() == ITestResult.FAILURE) {
+			System.out.println("Failed: " + testResult.getMethod().getMethodName());
+		}
+		if (testResult.getStatus() == ITestResult.SUCCESS) {
+			System.out.println("Successful: " + testResult.getName());
+		}
+	}
+}
