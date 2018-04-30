4ad8e27c689359ce2787383567f6cfa285be3a77
ostfale <info@jug-hh.de>
2018-04-30 08:17:24 +0200

    TestNG - Dataprovider
    + setup for a few data

---
+++ testfw/src/test/java/de/ostfale/testng/dataprovider/TestNG_DataProvider.java
@@ -0,0 +1,28 @@
+package de.ostfale.testng.dataprovider;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+/**
+ * Example for a data provider class
+ * Created : 30.04.2018
+ *
+ * @author : usauerbrei
+ */
+public class TestNG_DataProvider {
+
+	@DataProvider(name = "inputs")
+	public Object[][] getData() {
+		return new Object[][]{
+				{"bmw", "m3"},
+				{"audi", "a6"},
+				{"benz", "c300"}
+		};
+	}
+
+	@Test(dataProvider = "inputs")
+	public void testMethod1(String input1, String input2) {
+		System.out.println("Input 1: " + input1);
+		System.out.println("Input 2: " + input2);
+	}
+}
