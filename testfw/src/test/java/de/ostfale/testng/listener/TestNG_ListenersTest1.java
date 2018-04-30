45f969e1d09667e758a2c587d67ac268c6513114
ostfale <info@jug-hh.de>
2018-04-30 08:53:20 +0200

    TestNG - Intercept every method with a listener
    + add custom listener and test suite

---
+++ testfw/src/test/java/de/ostfale/testng/listener/TestNG_ListenersTest1.java
@@ -0,0 +1,40 @@
+package de.ostfale.testng.listener;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+/**
+ * Example test class where the methods are intercepted by a listener independently of the type of test or pre or
+ * post condition.
+ * Created : 30.04.2018
+ *
+ * @author : usauerbrei
+ */
+@Listeners(CustomListener1.class)
+public class TestNG_ListenersTest1 {
+
+	@BeforeClass
+	public void setUp() {
+		System.out.println("Code in before class");
+	}
+
+	@AfterClass
+	public void cleanUp() {
+		System.out.println("Code in after class");
+	}
+
+	@Test
+	public void testMethod1() {
+		System.out.println("Code in testMethod1");
+		Assert.assertTrue(true);
+	}
+
+	@Test
+	public void testMethod2() {
+		System.out.println("Code in testMethod2");
+		Assert.assertTrue(false);
+	}
+}
