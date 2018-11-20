f1d6c863aa37d7c54fdc4c895555b543aff22a63
ostfale <info@jug-hh.de>
2018-11-20 09:13:09 +0100

    Collector examples

---
+++ lambdastream/src/main/java/naftalin/streams/bookexample/CollectorsExample.java
@@ -0,0 +1,136 @@
+package naftalin.streams.bookexample;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.Year;
+import java.util.*;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.function.BinaryOperator;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
+
+/**
+ * Collectors functions
+ * Created : 16.11.2018
+ *
+ * @author : usauerbrei
+ */
+@Slf4j
+public class CollectorsExample {
+
+	private static List<Book> library = Bootstrap.getLibrary();
+
+	public static void main(String[] args) {
+		doIt();
+	}
+
+	private static void doIt() {
+		chainingMostPopularTopic();
+		mapConcatenatedTitlesByTopic();
+		partitioningByFictionOrNonFiction();
+		summarizeStatistice();
+		groupingByAveragedHeightPerTopic();
+		groupingVolumeCountsByTopic();
+		groupingByNumberOfAuthors();
+		groupingByAndCountingCollector();
+		groupingByWithUserSuppliedCollector();
+		groupingBooksByTopic();
+		toCollectionExamples();
+		toMapFunction();
+		joiningExamples();
+	}
+
+	private static void chainingMostPopularTopic() {
+		Optional<Topic> mostPopularTopic = library.stream()
+				                                   .collect(groupingBy(Book::getTopic, counting()))
+				                                   .entrySet().stream()
+				                                   .max(Map.Entry.comparingByValue())
+				                                   .map(Map.Entry::getKey);
+		log.info("Most popular topic: " + mostPopularTopic.get());
+	}
+
+	private static void mapConcatenatedTitlesByTopic() {
+		Map<Topic, String> concatenatedTitlesByTopic = library.stream()
+				                                               .collect(groupingBy(Book::getTopic, mapping(Book::getTitle, joining(";"))));
+		log.info("Concatenated titles per topic: " + concatenatedTitlesByTopic);
+	}
+
+	private static void summarizeStatistice() {
+		Map<Topic, IntSummaryStatistics> volumeStats = library.stream()
+				                                               .collect(groupingBy(Book::getTopic, summarizingInt(b -> b.getPageCount().length)));
+		log.info("Show volume statistics: " + volumeStats);
+	}
+
+	private static void groupingByAveragedHeightPerTopic() {
+		Map<Topic, Double> averageHeightByTopic = library.stream()
+				                                          .collect(groupingBy(Book::getTopic, averagingDouble(Book::getHeight)));
+		log.info("Average height per Topic: " + averageHeightByTopic);
+	}
+
+	private static void groupingVolumeCountsByTopic() {
+		Map<Topic, Integer> volumeCountByTopic = library.stream()
+				                                         .collect(groupingBy(Book::getTopic, summingInt(b -> b.getPageCount().length)));
+		log.info("Volumes per topic: " + volumeCountByTopic);
+	}
+
+	private static void groupingByNumberOfAuthors() {
+		Map<Topic, Optional<Book>> mostAuthorsByTopic = library.stream()
+				                                                .collect(groupingBy(Book::getTopic, maxBy(Comparator.comparing(b -> b.getAuthors().size()))));
+		log.info("Books by no of authors: " + mostAuthorsByTopic);
+	}
+
+	private static void groupingByAndCountingCollector() {
+		Map<Topic, Long> booksCounted = library.stream().collect(groupingBy(Book::getTopic, Collectors.counting()));
+		log.info("GroupingBy with user Collectors counting: " + booksCounted);
+	}
+
+	private static void groupingByWithUserSuppliedCollector() {
+		Map<Topic, List<Book>> booksByTopic = library.stream().collect(groupingBy(Book::getTopic, Collectors.toList()));
+		log.info("GroupingBy with user supplied collector: " + booksByTopic);
+	}
+
+	private static void partitioningByFictionOrNonFiction() {
+		Map<Boolean, List<Book>> fictionOrNonFiction = library.stream()
+				                                               .collect(partitioningBy(b -> b.getTopic() == Topic.FICTION || b.getTopic() == Topic.HISTORY));
+		log.info("Partitioning by Fiction or Non Fiction: " + fictionOrNonFiction);
+	}
+
+	private static void groupingBooksByTopic() {
+		Map<Topic, List<Book>> booksByTopic = library.stream()
+				                                      .collect(groupingBy(Book::getTopic));
+		log.info("Grouped books by Topic: " + booksByTopic);
+	}
+
+	private static void toCollectionExamples() {
+
+		NavigableSet<String> sortedTitles = library.stream().map(Book::getTitle).collect(toCollection(TreeSet::new));
+		log.info("Collect into navigable set: " + sortedTitles);
+
+		BlockingQueue<Book> queueInPubDateOrder = library.stream()
+				                                          .sorted(Comparator.comparing(Book::getPubDate))
+				                                          .collect(toCollection(LinkedBlockingQueue::new));
+		log.info("Collect in blocking queue: " + queueInPubDateOrder);
+
+	}
+
+	private static void toMapFunction() {
+		Map<String, Year> titleToPubDate = library.stream()
+				                                   .collect(toMap(Book::getTitle,
+						                                   Book::getPubDate,
+						                                   BinaryOperator.maxBy(Comparator.naturalOrder()), TreeMap::new));
+		log.info("Books by PubDate: " + titleToPubDate);
+	}
+
+	private static void joiningExamples() {
+		String concatenatedTitles = library.stream().map(Book::getTitle).collect(joining("::"));
+		log.info("Concatenated titles: {}", concatenatedTitles);
+
+		List<String> allAuthorsForABook = library.stream()
+				                                  .map(b -> b.getAuthors().stream()
+						                                            .collect(joining(" - ", b.getTitle() + ": ", "")))
+				                                  .collect(toList());
+		log.info("Books by authors: " + allAuthorsForABook.toString());
+	}
+}
--- lambdastream/src/main/java/naftalin/streams/bookexample/ExamplesStreamProcessing.java
+++ lambdastream/src/main/java/naftalin/streams/bookexample/ExamplesStreamProcessing.java
@@ -2,12 +2,13 @@ package naftalin.streams.bookexample;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.time.Year;
+import java.util.*;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.toSet;
 
 /**
  * Examples of stream processing using book library data
@@ -50,7 +51,44 @@ public class ExamplesStreamProcessing {
 		Optional<Book> oldestBook = library.stream().min(Comparator.comparing(book -> book.getPubDate()));
 		log.info("Oldest book: {}", oldestBook.get());
 
-		Set<String> titles = library.stream().map(Book::getTitle).collect(Collectors.toSet());
+		Set<String> titles = library.stream().map(Book::getTitle).collect(toSet());
+
+		intSummaryStatisticsExample();
+		findOldestBookInLibrary();
+		collectTitleOfBooksIntoSet();
+		mapBookTitleToPublicationDate();
+		mappingWithDuplicateKeysAllowed();
+	}
+
+	private static void mappingWithDuplicateKeysAllowed() {
+		Map<String, Year> titleToPubdate = library.stream()
+				                                   .collect(toMap(Book::getTitle, Book::getPubDate, (x, y) -> x.isAfter(y) ? x : y));
+		log.info("Map with merger function: " + titleToPubdate.toString());
+	}
+
+	private static void mapBookTitleToPublicationDate() {
+		Map<String, Year> titleToPubDate = library.stream()
+				                                   .collect(toMap(Book::getTitle, Book::getPubDate));
+		log.info("Mapped title to publication date: " + titleToPubDate.toString());
+	}
+
+	private static void collectTitleOfBooksIntoSet() {
+		Set<String> titles = library.stream().map(Book::getTitle).collect(toSet());
+		log.info("All book titles: {}", titles.toString());
+	}
+
+	private static void findOldestBookInLibrary() {
+		Optional<Book> oldestBook = library.stream()
+				                            .min(Comparator.comparing(Book::getPubDate));
+		log.info("Oldest book in library: {}", oldestBook.get().getTitle());
+	}
+
+	private static void intSummaryStatisticsExample() {
+
+		IntSummaryStatistics intSummaryStatistics = library.stream()
+				                                            .mapToInt(b -> IntStream.of(b.getPageCount()).sum())
+				                                            .summaryStatistics();
+		log.info("Pagecount: {} :: Min: {} :: Max: {}", intSummaryStatistics.getCount(), intSummaryStatistics.getMin(), intSummaryStatistics.getMax());
 
 	}
 
